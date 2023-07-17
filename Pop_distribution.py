716975299f59f05d5d04477f3611c5a72e3c9736
prvnpandey <prvnpandey2010@gmail.com>
2023-07-16 23:35:23 -0400

    adding arrivaal rate back to city

--- Pop_distribution.py
+++ Pop_distribution.py
@@ -5,13 +5,7 @@ import matplotlib.pyplot as plt
 def arrival_rate_back_to_city():
     plt.cla()
     random.seed(1)
-    
-    pass
-
-def departure_rate_for_hoilday():
-    plt.cla()
-    random.seed(1)
-    mu, var, samples = 12, 2.3, 30
+    mu, var, samples = 9, 2, 30
     sample_pop = np.random.normal(mu, var, samples)
     sample_pop.sort()
     even_index = list(range(0, sample_pop.size, 2))
@@ -20,7 +14,29 @@ def departure_rate_for_hoilday():
     index = even_index + odd_index
     # samples of pop according to gaussian normal distribution
     samples = sample_pop[index]
-
-    plt.plot(samples)
-    plt.show()
     return samples
+
+def departure_rate_for_hoilday():
+    plt.cla()
+    random.seed(1)
+    mu, var, samples = 12.5, 2.3, 30
+    sample_pop = np.random.normal(mu, var, samples)
+    sample_pop.sort()
+    even_index = list(range(0, sample_pop.size, 2))
+    odd_index = list(range(1, sample_pop.size, 2))
+    odd_index.reverse()
+    index = even_index + odd_index
+    # samples of pop according to gaussian normal distribution
+    samples = sample_pop[index]
+    return samples
+
+df1 = departure_rate_for_hoilday()
+df2= arrival_rate_back_to_city()
+plt.plot(df1, label='departure_rate')
+plt.plot(df2,label='arrival_rate')
+plt.xlabel(' Day of a month')
+plt.ylabel(' Fraction of population leaving and coming back to city')
+plt.legend(loc='best')
+plt.savefig('dep_arrival_rate1.png')
+plt.show()
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import Pop_distribution
-import simpy
+# import simpy
 
 class initial_scenario():
     def __init__(self, population, fleet, passenger_per_vehicle):
@@ -7,15 +7,22 @@ class initial_scenario():
         self.fleet = fleet
         self.passenger_per_vehicle = passenger_per_vehicle
 
-def scenario_1(scenario, fraction_of_pop_holiday):
+def scenario_1(scenario, fraction_of_pop_to_holiday,fraction_of_pop_from_holiday):
     fleet = scenario.fleet
     population = scenario.population
-    for daily_fraction in fraction_of_pop_holiday:
-        pop_on_holidayTrips = round(population*(daily_fraction/100))
-        required_fleet_for_hoildayTrips = round(pop_on_holidayTrips/scenario.passenger_per_vehicle)
-        population = population-pop_on_holidayTrips
+    additional_fleet =[]
+    for day in range(0,fraction_of_pop_to_holiday.size):
+        pop_on_holidayTrips = round(population*(fraction_of_pop_to_holiday[day]/100))
+        if(day>3):
+            pop_back_to_city = round(population*(fraction_of_pop_from_holiday[day]/100))
+        else:
+            pop_back_to_city = 0
+        required_fleet_for_hoildayTrips = round((pop_on_holidayTrips-pop_back_to_city)/scenario.passenger_per_vehicle)
+        population = population-pop_on_holidayTrips +pop_back_to_city
         fleet = fleet - required_fleet_for_hoildayTrips
-        fleet = fleet + round((population*0.1 - fleet))
+        additional_fleet.append(round((population*0.1 - fleet)))
+        fleet = fleet + additional_fleet[day]
+
     return fleet, population, scenario.population-population, (scenario.population-population)/scenario.passenger_per_vehicle
 
 
@@ -23,14 +30,12 @@ if __name__ == '__main__':
     #population = int(input('enter the population in city, must be an integer value'))
     population = 100000
     scenario = initial_scenario(population, population*0.1, 2 )
-    fraction_of_pop_holiday = Pop_distribution.departure_rate_for_hoilday() # on daily basis
-    fleet, population, hoilday, hoilday_fleet = scenario_1(scenario, fraction_of_pop_holiday)
+    fraction_of_pop_to_holiday = Pop_distribution.departure_rate_for_hoilday() # on daily basis
+    fraction_of_pop_from_holiday = Pop_distribution.arrival_rate_back_to_city()
+    fleet, population, hoilday, hoilday_fleet = scenario_1(scenario, fraction_of_pop_to_holiday, fraction_of_pop_from_holiday)
     print(f'The Population in city is {population}, with a fleet size of {fleet}')
     print(f'The Population on hoilday is {hoilday}, with a fleet size of {hoilday_fleet}')
     print(f'Total required fleet with hoilday scneario is {fleet+hoilday_fleet}')
-
-
-
 
 
 
