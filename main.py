cb30f7e4acafc7303cabfcf5d48b176f76c946ec
prvnpandey <prvnpandey2010@gmail.com>
2023-07-17 00:27:04 -0400

    adding arrivaal rate back to city

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 import Pop_distribution
 # import simpy
 
@@ -13,7 +15,7 @@ def scenario_1(scenario, fraction_of_pop_to_holiday,fraction_of_pop_from_holiday
     additional_fleet =[]
     for day in range(0,fraction_of_pop_to_holiday.size):
         pop_on_holidayTrips = round(population*(fraction_of_pop_to_holiday[day]/100))
-        if(day>3):
+        if(day>0):
             pop_back_to_city = round(population*(fraction_of_pop_from_holiday[day]/100))
         else:
             pop_back_to_city = 0
@@ -23,7 +25,7 @@ def scenario_1(scenario, fraction_of_pop_to_holiday,fraction_of_pop_from_holiday
         additional_fleet.append(round((population*0.1 - fleet)))
         fleet = fleet + additional_fleet[day]
 
-    return fleet, population, scenario.population-population, (scenario.population-population)/scenario.passenger_per_vehicle
+    return fleet, population, scenario.population-population, (scenario.population-population)/scenario.passenger_per_vehicle, additional_fleet
 
 
 if __name__ == '__main__':
@@ -32,10 +34,11 @@ if __name__ == '__main__':
     scenario = initial_scenario(population, population*0.1, 2 )
     fraction_of_pop_to_holiday = Pop_distribution.departure_rate_for_hoilday() # on daily basis
     fraction_of_pop_from_holiday = Pop_distribution.arrival_rate_back_to_city()
-    fleet, population, hoilday, hoilday_fleet = scenario_1(scenario, fraction_of_pop_to_holiday, fraction_of_pop_from_holiday)
+    fleet, population, hoilday, hoilday_fleet,additional_fleet_per_day = scenario_1(scenario, fraction_of_pop_to_holiday, fraction_of_pop_from_holiday)
     print(f'The Population in city is {population}, with a fleet size of {fleet}')
     print(f'The Population on hoilday is {hoilday}, with a fleet size of {hoilday_fleet}')
     print(f'Total required fleet with hoilday scneario is {fleet+hoilday_fleet}')
+
 
 
 
